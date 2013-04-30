116bbcc9e312341ab2c8a9ce91412bcd91a8ea7c
Brett Hoyer <jbh5079@gmail.com>
2013-04-29 19:02:25 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frugal::Application.config.secret_token = '9e1b17d2cfff748aa62521d2004d339ace4110a7fad1fcd76d637581e85642a966cdc8b38d0d1c692b55950348b3df00db152f226d48a59033b6d51f1b8fd9e7'
